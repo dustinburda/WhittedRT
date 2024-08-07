67a1c08a46fbdf13922c3a96e778c0bc5a379107
dustinburda <dustin99wii@gmail.com>
2024-08-07 01:36:50 -0400

    added canvas

---
+++ include/Canvas.h
@@ -0,0 +1,16 @@
+//
+// Created by Dustin on 8/7/24.
+//
+
+#ifndef CANVAS_H
+#define CANVAS_H
+
+
+
+class Canvas {
+
+};
+
+
+
+#endif //CANVAS_H
---
+++ src/Canvas.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dustin on 8/7/24.
+//
+
+#include "../include/Canvas.h"
