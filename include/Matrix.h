1db5b436371d7649f37280c81092d563bfd449d8
Dustin <dustin99wii@gmail.com>
2024-11-30 02:48:23 -0500

    More matrix class

--- include/Matrix.h
+++ include/Matrix.h
@@ -7,7 +7,10 @@
 
 #include <array>
 #include <cstdint>
+#include <cstring>
 #include <initializer_list>
+#include <stdexcept>
+#include <string>
 
 #include "Normal.h"
 #include "Point.h"
@@ -17,22 +20,70 @@
 template<size_t M, size_t N, typename T>
 class Matrix {
 public:
-    Matrix() = default;
-    Matrix(std::initializer_list<T> data) {}
+    Matrix() {
+        for(int i = 0; i < M; i++)
+            std::memset(data_[i].data(), 0, sizeof (T) * N);
+    }
 
-    T operator[](std::size_t index);
+    Matrix(std::initializer_list<T> data) {
+        if (data.size() != M * N)
+            throw std::logic_error("Must initialize a " + std::to_string(M) +  " by " + std::to_string(N) +
+                                    " matrix with " + std::to_string(M*N) + " elements!");
 
-    Matrix<M, N, T>& operator-();
-    Matrix<M, N, T> operator-() const;
+        for(int i = 0; i < M; i++)
+            for(int j = 0; j < N; j++)
+                data_[i][j] = data[i][j];
+    }
+
+    std::array<T, N>& operator[](std::size_t index) {
+        if(index >= M || index < 0)
+            throw std::logic_error("You must access an element within bounds!");
+
+        return data_[index];
+    }
+
+    Matrix<M, N, T>& operator-() {
+        for(int i = 0; i < M; i++)
+            for(int j = 0; j < N; j++)
+                data_[i][j] *= -1;
+
+        return *this;
+    }
+    Matrix<M, N, T> operator-() const {
+        Matrix<M, N, T> negative;
+
+        for(int i = 0; i < M; i++)
+            for(int j = 0; j < N; j++)
+                negative[i][j] *= -data_[i][j];
+
+        return negative;
+    }
 
     Matrix<M, N, T>& operator+=(const Matrix& m);
+    Matrix<M, N, T>& operator-=(const Matrix& m);
     Matrix<M, N, T>& operator*(double t);
+    Matrix<M, N, T>& operator/(double t);
+
+
 
 
 private:
     std::array<std::array<T, N>, M> data_;
 };
+template<size_t M, size_t N, typename T>
+bool operator==(const Matrix<M, N, T>& m1, const Matrix<M, N, T>& m2);
 
+template<size_t M, size_t N, typename T>
+bool Invertible(const Matrix<M, N, T> m);
+
+template<size_t M, size_t N, typename T>
+Matrix<M, N, T> Transpose(const Matrix<M, N, T> m);
+
+template<size_t M, size_t N, typename T>
+Matrix<M, N, T> Determinant(const Matrix<M, N, T> m);
+
+template<size_t M, size_t N, typename T>
+Matrix<M, N, T> Inverse(const Matrix<M, N, T> m);
 
 template<size_t M, size_t N, typename T>
 Matrix<M, N, T> operator+(const Matrix<M, N, T>& m1, const Matrix<M, N, T>& m2);
