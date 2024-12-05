306ff1b0c2b7c10a91d6ff4e27e1bea1c65e1063
Dustin <dustin99wii@gmail.com>
2024-12-04 23:55:15 -0500

    Color class

--- include/Color.h
+++ include/Color.h
@@ -5,10 +5,17 @@
 #ifndef WHITTED_COLOR_H
 #define WHITTED_COLOR_H
 
-
+#include <cstdint>
 
 class Color {
+public:
+    Color() : color_{0x00000000} {};
+    Color(std::uint8_t R, std::uint8_t G, std::uint8_t B) {
+        color_ = 0x00000000;
 
+    }
+private:
+    std::uint32_t color_;
 };
 
 
