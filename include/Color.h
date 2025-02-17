604615c108c2cdf39cd7536241d3ffc6ab988ff4
dustinburda <dustin99wii@gmail.com>
2025-02-17 12:05:37 -0500

    Get rid of static functions in header files that aren't templated

--- include/Color.h
+++ include/Color.h
@@ -81,51 +81,13 @@ private:
     std::array<double, 4> data_;
 };
 
-static bool operator==(const Color& c1, const Color& c2) {
-    return std::abs(c1.R() - c2.R()) < epsilon && std::abs(c1.G() - c2.G()) < epsilon  && std::abs(c1.B() - c2.B()) < epsilon;
-}
-
-static bool operator!=(const Color& c1, const Color& c2) {
-    return !(c1 == c2);
-}
-
-static Color operator+(const Color& c1, const Color& c2) {
-    Color c;
-
-    c.R() = c1.R() + c2.R();
-    c.G() = c1.G() + c2.G();
-    c.B() = c1.B() + c2.B();
-
-    return c;
-}
-
-static Color operator*(const Color& c1, const Color& c2) {
-    Color c;
-
-    c.R() = c1.R() * c2.R();
-    c.G() = c1.G() * c2.G();
-    c.B() = c1.B() * c2.B();
-
-    return c;
-}
-
-static Color operator*(const Color& c1, double t) {
-    Color c;
-
-    c.R() = c1.R() * t;
-    c.G() = c1.G() * t;
-    c.B() = c1.B() * t;
-
-    return c;
-}
-
-static Color operator*(double t, const Color& c1) {
-    return c1 * t;
-}
-
-static Color operator/(const Color& c1, double t) {
-    return c1 * (1/t);
-}
+bool operator==(const Color& c1, const Color& c2);
+bool operator!=(const Color& c1, const Color& c2);
+Color operator+(const Color& c1, const Color& c2);
+Color operator*(const Color& c1, const Color& c2);
+Color operator*(const Color& c1, double t);
+Color operator/(const Color& c1, double t);
+Color operator*(double t, const Color& c1);
 
 
 
--- src/Color.cpp
+++ src/Color.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "../include/Color.h"
+
+bool operator==(const Color& c1, const Color& c2) {
+    return std::abs(c1.R() - c2.R()) < epsilon && std::abs(c1.G() - c2.G()) < epsilon  && std::abs(c1.B() - c2.B()) < epsilon;
+}
+
+bool operator!=(const Color& c1, const Color& c2) {
+    return !(c1 == c2);
+}
+
+Color operator+(const Color& c1, const Color& c2) {
+    Color c;
+
+    c.R() = c1.R() + c2.R();
+    c.G() = c1.G() + c2.G();
+    c.B() = c1.B() + c2.B();
+
+    return c;
+}
+
+Color operator*(const Color& c1, const Color& c2) {
+    Color c;
+
+    c.R() = c1.R() * c2.R();
+    c.G() = c1.G() * c2.G();
+    c.B() = c1.B() * c2.B();
+
+    return c;
+}
+
+Color operator*(const Color& c1, double t) {
+    Color c;
+
+    c.R() = c1.R() * t;
+    c.G() = c1.G() * t;
+    c.B() = c1.B() * t;
+
+    return c;
+}
+
+Color operator*(double t, const Color& c1) {
+    return c1 * t;
+}
+
+Color operator/(const Color& c1, double t) {
+    return c1 * (1/t);
+}
