8f09ecaf363024d40ea7e2c521784b914f25643b
dustinburda <dustin99wii@gmail.com>
2025-04-10 23:51:55 -0400

    Partial texture implementation

--- include/Texture.h
+++ include/Texture.h
@@ -5,18 +5,45 @@
 #ifndef WHITTED_TEXTURE_H
 #define WHITTED_TEXTURE_H
 
+#include "Color.h"
+#include "Point.h"
 
 #include "../lib/stb_image.h"
 
 #include <filesystem>
 #include <fstream>
+#include <vector>
 
 class Texture {
 public:
-
-private:
-
+    virtual ~Texture();
+    virtual Color GetTexel(Point2d texture_coordinates) = 0;
 };
+
+class ImageTexture : public Texture {
+public:
+    ImageTexture(std::string& filename) {
+        int x;
+        int y;
+        unsigned char* bitmap_data = stbi_load(filename.data(), &x, &y, nullptr, 3);
+
+        num_cols_ = x;
+        num_rows_ = y;
+
+        stbi_image_free(bitmap_data);
+    }
+
+    Color GetTexel(Point2d texture_coordinates) override {
+        return Color{};
+    }
+
+    ~ImageTexture() override;
+private:
+    std::vector<std::vector<Color>> bitmap_;
+    std::size_t num_rows_;
+    std::size_t num_cols_;
+};
+
 
 
 #endif //WHITTED_TEXTURE_H
