5b6d521e895f6d895b9ebcdbb4bcf0e3293ff1c9
dustinburda <dustin99wii@gmail.com>
2026-01-26 00:09:29 -0500

    Drawable box added

---
+++ include/Box.h
@@ -0,0 +1,14 @@
+//
+// Created by Dustin on 1/26/26.
+//
+
+#ifndef WHITTED_BOX_H
+#define WHITTED_BOX_H
+
+#include "ShapeInterface.h"
+
+class Box : public ShapeInterface {
+
+};
+
+#endif //WHITTED_BOX_H
---
+++ src/Box.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dustin on 1/26/26.
+//
+
+#include "../include/Box.h"
